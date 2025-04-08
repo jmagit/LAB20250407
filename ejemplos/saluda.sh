3e8064313fdaf1b7aaee97c660e4a61790b94b3d
Javier <35864946+jmagit@users.noreply.github.com>
2025-04-08 10:39:55 +0200

    old

--- ejemplos/saluda.sh
+++ ejemplos/saluda.sh
@@ -3,11 +3,10 @@
 NOMBRE=$1
 MODO=$2
 MOSTRAR=$3
+
 if [ "$MOSTRAR" = "true" ]; then
-	echo "Hola, $NOMBRE";
-else
-	echo "Hola, mundo";
+	echo "Hola, $NOMBRE"
 fi
 echo "Se ha ejecutado el script en modo $MODO"
 
-# ejemplo de comando: exec /home/saluda.sh $NOMBRE $MODO $MOSTRAR
+# comando: exec /home/saluda.sh $NOMBRE $MODO $MOSTRAR
