ee5aac22718c0f0ded9f07d6b78c1da0d574ecae
Javier <35864946+jmagit@users.noreply.github.com>
2025-04-08 10:25:46 +0200

    cambia sh

--- ejemplos/saluda.sh
+++ ejemplos/saluda.sh
@@ -5,8 +5,10 @@ MODO=$2
 MOSTRAR=$3
 
 if [ "$MOSTRAR" = "true" ]; then
-	echo "Hola, $NOMBRE"
+	echo "Hola, $NOMBRE";
+else
+	echo "Hola, mundo";
 fi
 echo "Se ha ejecutado el script en modo $MODO"
 
-# comando: exec /home/saluda.sh $NOMBRE $MODO $MOSTRAR
+# ejemplo de comando: exec /home/saluda.sh $NOMBRE $MODO $MOSTRAR
